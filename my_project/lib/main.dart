8d49b4ed02f3abf48e089a467eab6928c394e5b5
Bhavin Aghera <iambhavinaghera@gmail.com>
2023-08-22 22:05:40 +0530

    home Page Screen Add

--- my_project/lib/main.dart
+++ my_project/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_project/LogIn.dart';
 import 'package:my_project/Screen3.dart';
 import 'package:my_project/Screen4.dart';
+import 'package:my_project/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,26 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: LogIn(),
+      home: HomePage(),
     );
   }
 }
